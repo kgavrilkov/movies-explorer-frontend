2a53c06025c5f0efefc67e65e1b6c12620240d97
Kirill Gavrilkov <kgavrilkov@yandex.ru>
2021-06-29 20:48:53 +0300

    fix: SearchForm switch-button

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,18 +2,25 @@ import React from 'react';
 import './SearchForm.css';
 
 function SearchForm() {
+  const [isSwitched, setIsSwitched] = React.useState(false);
+
+  function handleSwitchClick() {
+    setIsSwitched(!isSwitched);
+  }
+
+  const className = `finder__switch-button ${isSwitched ? 'finder__switch-button_active' : 'finder__switch-button'}`;
+
   return(
     <section className="finder">
       <div className="container container_presentation">
-        <form className="finder__form">
+        <form className="finder__form" noValidate>
           <input type="text" className="finder__input" required placeholder="Фильм" />
           <button type="submit" className="finder__button"></button>
         </form>
-        <label className="finder__label">
-          <input type="checkbox" className="finder__checkbox" value="short-movie" />
-          <span className="finder__pseudo-item"></span>
-          <span className="finder__label-text">Короткометражки</span>
-        </label>
+        <div className="finder__switch-section">
+          <button className={className} onClick={handleSwitchClick}></button>
+          <span className="finder__switch-section-title">Короткометражки</span>
+        </div>
         <hr className="finder__line" />
       </div>
     </section>
